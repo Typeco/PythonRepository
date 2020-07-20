4fa48f48b0694eca5aaac75b9a97e8f1edd24d0a
Typeco <40288523+Typeco@users.noreply.github.com>
2020-07-20 22:02:39 +0800

    03

---
+++ 03.py
@@ -0,0 +1,132 @@
+
+from herolist import *
+from combo import *
+
+##################################
+#需要将羁绊统一归零
+def clear(n):
+    fnow = {}
+    for i in range(len(n)):
+        fnow[n[i]] = 0
+        i += 1
+    return fnow
+
+jiban = clear(nlist)
+##################################
+##################################
+
+##############
+#输入数字
+n = int(input("Hero number: ").strip())
+
+#遍历所有英雄
+nowhero = []
+for i in range(n):
+    x = input("Hero{} name: ".format(str(i + 1)))     #输入英雄名称
+    nowhero.append(x)
+    #将英雄身上所有羁绊记录
+    def addp(xp):
+        jiban[xp] += 1
+        return
+    #pn 羁绊数(三项属性-第一项)
+    pn = len(herolist[x]) - 1
+    for o in range(pn):
+        p = 'p' + str(o + 1)
+        addp(herolist[x][p])
+
+
+
+#检索剔除羁绊数为0
+for i in nlist:
+    if jiban[i] == 0:
+        del jiban[i]
+##################################
+print(jiban)
+
+for i in jiban:
+    print("羁绊\"{}\":{}".format(i,jiban[i]))
+
+
+
+def closenum(jiban,combonum):    
+    f = []
+    weig = []    
+    for key in jiban:
+        x = []
+        lev = 0
+        #########   
+        #计算差值
+        for i in combonum[key]:         
+            x.extend([combonum[key][i] - jiban[key]])                          
+        #判断x中的负数        
+        for o in range(len(x)):            
+            if x[o] <= 0:
+                x[o] = 100        
+                #计算权重
+                lev += 1 
+        #寻找x中最小值
+        weig.append(lev)
+        f.extend([min(x)])              
+#         print(x,lev) 
+
+    return f,weig 
+        
+
+
+
+www = closenum(jiban,combonum)
+print("还差羁绊{}，当前羁绊等级{}".format(www[0],www[1]))
+
+
+
+#查找羁绊
+def findHeroPro(dict,x):
+    dictlist = list(dict.values())
+    a = []
+    for i in dictlist:
+        for key, value in i.items():
+            if value == x :
+                a.append(list(dict.keys())[list(dict.values()).index(i)])
+    return a
+
+needhero = []
+for i in jiban.keys():
+    needhero.extend(findHeroPro(herolist,i))
+
+# x = "斗士"
+# needhero = findHeroPro(herolist,x)
+
+
+print("查找羁绊:", jiban.keys())
+
+#剔除拥有的英雄
+for i in needhero:
+    if i in nowhero:
+        needhero.remove(i)
+
+
+print("Need hero name: ",needhero)
+
+b = []
+for i in needhero:
+    b.append(herolist[i]['star'])
+print("Hero's Star: ",b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
